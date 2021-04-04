3a72cac38c039cc7db7bad231c1720962edff3f5
GD Kimaiyo <gdkimaiyo@gmail.com>
2021-04-04 11:07:25 +0300

    more file io examples

--- src/26.file_io.c
+++ src/26.file_io.c
@@ -1,12 +1,16 @@
-// Creating and Writing files in C
-// Read, Write, Append
+// Writing and Reading files in C
+// Append
 
 #include <stdio.h>
 
 int main(void) {
-  FILE *file = fopen("26.phonebook.csv", "a");
+  FILE *file;
+  char fname[50] = "26.phonebook.csv";
+  // Open file in appending mode
+  file = fopen(fname, "a");
 
   if (file == NULL) {
+    printf("Error while opening file!\n");
     return 1;
   }
 
@@ -17,10 +21,12 @@ int main(void) {
   scanf("%s", name);
   printf("Number: ");
   scanf("%s", number);
-  
-  // Write file
-  fprintf(file, "name,phone\n");
+
+  // Write file in appending mode
+  // fprintf(file, "name,phone\n");
   fprintf(file, "%s,%s\n", name, number);
   // Close file
   fclose(file);
+
+  printf("%s successfully updated\n", fname);
 }
---
+++ src/26.file_io_read.c
@@ -0,0 +1,32 @@
+// Writing and Reading files in C
+// Read
+
+#include <stdio.h>
+
+int main(void) {
+  FILE *file;
+  char fname[50];
+  char str;
+
+  // Prompt for file to open
+  printf("Which file to open?: ");
+  scanf("%s", fname);
+
+  // Open file in appending mode
+  file = fopen(fname, "r");
+
+  if (file == NULL) {
+    printf("Error while opening file or file does not exist...\n");
+    return 1;
+  }
+
+  printf("%s content\n\n", fname);
+  str = fgetc(file);
+  while (str != EOF) {
+    printf("%c", str);
+    str = fgetc(file);
+  }
+  printf("\n");
+  // Close file if it's open
+  fclose(file);
+}
