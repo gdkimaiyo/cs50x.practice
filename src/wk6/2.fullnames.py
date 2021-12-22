e0d54ba42dc1d15cb2a6605e84c7dabac2bb2bfe
gdkimaiyo <gdkimaiyo@gmail.com>
2021-12-22 12:48:20 +0300

    append lastname to firstnames in an array

---
+++ src/wk6/2.fullnames.py
@@ -0,0 +1,10 @@
+children = ["David", "June", "Jacob", "Sharon", "Justus"]
+lastname = "Malan"
+
+fullnames = []
+
+for firstname in children:
+    fullname = firstname + ' ' + lastname
+    fullnames.append(fullname)
+
+print(fullnames)
